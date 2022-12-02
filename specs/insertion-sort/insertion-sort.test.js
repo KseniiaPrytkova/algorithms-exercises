e2fd3b346b7b0d73c73539b4b9223fdd14905510
Kseniia Prytkova <prytkovakseniia@gmail.com>
2022-12-02 18:41:59 +0000

    add indertion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -14,13 +14,25 @@
 */
 
 function insertionSort(nums) {
-  // code goes here
+	for (let i = 1; i < nums.length; i++) {
+		for (let j = i; j > 0; j--) {
+			if (nums[j] < nums[j - 1]) {
+				let buff = nums[j];
+				nums[j] = nums[j - 1];
+				nums[j - 1] = buff;
+			} else {
+				break;
+			}
+		}
+	}
+
+	return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
-  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-  insertionSort(nums);
-  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+test("insertion sort", function () {
+	const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+	insertionSort(nums);
+	expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
