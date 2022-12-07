c35bb4c417e2ddbe1fcf8766e9d6d5011b9bbe47
Kseniia Prytkova <prytkovakseniia@gmail.com>
2022-12-07 16:16:02 +0000

    add ArrayList

--- specs/arrays/arraylist.test.js
+++ specs/arrays/arraylist.test.js
@@ -17,62 +17,112 @@
   you work
 */
 
+// STACK (true stack also has peak etc)
+// not checking for errors etc
 class ArrayList {
-  // code goes here
+	// evertime I hit new ArrayList() - the constructor() method gets run
+	constructor() {
+		// instantiate all your variables
+		this.data = {};
+		this.length = 0;
+	}
+
+	push(value) {
+		// add at item to the end of the array
+		this.data[this.length] = value;
+		this.length++;
+	}
+
+	pop() {
+		// remove the last item in the array and returns it
+		const response = this.data[this.length - 1];
+		delete this.data[this.length - 1];
+		this.length--;
+
+		// or:
+		// return this.delete(this.length - 1)
+
+		return response;
+	}
+
+	// the whole REASON for ArrayList: get() is very fast and simple
+	get(index) {
+		// returns that item from the array
+		// check bounds! and then:
+		return this.data[index];
+	}
+
+	delete(index) {
+		// removes item from the array and collapses (shift everything over) the array
+		const response = this.data[index];
+
+		this._collapseTo(index);
+
+		return response;
+	}
+
+	_collapseTo(index) {
+		for (let i = index; i < this.length; i++) {
+			this.data[i] = this.data[i + 1];
+		}
+
+		delete this.data[this.length - 1];
+		this.length--;
+	}
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("ArrayList", function () {
-  const range = (length) =>
-    Array.apply(null, { length: length }).map(Number.call, Number);
-  const abcRange = (length) =>
-    range(length).map((num) => String.fromCharCode(97 + num));
-  let list;
+describe("ArrayList", function () {
+	const range = (length) =>
+		Array.apply(null, { length: length }).map(Number.call, Number);
+	const abcRange = (length) =>
+		range(length).map((num) => String.fromCharCode(97 + num));
+	let list;
 
-  beforeEach(() => {
-    list = new ArrayList();
-  });
+	beforeEach(() => {
+		list = new ArrayList();
+	});
 
-  test("constructor", () => {
-    expect(list).toEqual(expect.any(ArrayList));
-  });
+	test("constructor", () => {
+		expect(list).toEqual(expect.any(ArrayList));
+	});
 
-  test("push", () => {
-    abcRange(26).map((character) => list.push(character));
-    expect(list.length).toEqual(26);
-  });
+	test("push", () => {
+		abcRange(26).map((character) => list.push(character));
+		expect(list.length).toEqual(26);
+	});
 
-  test("pop", () => {
-    abcRange(13).map((character) => list.push(character));
-    expect(list.length).toEqual(13);
-    range(10).map(() => list.pop());
-    expect(list.length).toEqual(3);
-    expect(list.pop()).toEqual("c");
-  });
+	test("pop", () => {
+		abcRange(13).map((character) => list.push(character));
+		expect(list.length).toEqual(13);
+		range(10).map(() => list.pop());
+		expect(list.length).toEqual(3);
+		expect(list.pop()).toEqual("c");
+	});
 
-  test("get", () => {
-    list.push("first");
-    expect(list.get(0)).toEqual("first");
-    list.push("second");
-    expect(list.get(1)).toEqual("second");
-    expect(list.get(0)).toEqual("first");
-    abcRange(26).map((character) => list.push(character));
-    expect(list.get(27)).toEqual("z");
-    expect(list.get(0)).toEqual("first");
-    expect(list.get(9)).toEqual("h");
-    list.pop();
-    expect(list.get(list.length - 1)).toEqual("y");
-  });
+	test("get", () => {
+		list.push("first");
+		expect(list.get(0)).toEqual("first");
+		list.push("second");
+		expect(list.get(1)).toEqual("second");
+		expect(list.get(0)).toEqual("first");
+		abcRange(26).map((character) => list.push(character));
+		expect(list.get(27)).toEqual("z");
+		expect(list.get(0)).toEqual("first");
+		expect(list.get(9)).toEqual("h");
+		list.pop();
+		expect(list.get(list.length - 1)).toEqual("y");
+	});
 
-  test("delete", () => {
-    abcRange(26).map((character) => list.push(character));
-    list.delete(13);
-    expect(list.length).toEqual(25);
-    expect(list.get(12)).toEqual("m");
-    expect(list.get(13)).toEqual("o");
-    list.delete(0);
-    expect(list.length).toEqual(24);
-    expect(list.get(0)).toEqual("b");
-  });
+	test("delete", () => {
+		abcRange(26).map((character) => list.push(character));
+		list.delete(13);
+		expect(list.length).toEqual(25);
+		expect(list.get(12)).toEqual("m");
+		expect(list.get(13)).toEqual("o");
+		list.delete(0);
+		expect(list.length).toEqual(24);
+		expect(list.get(0)).toEqual("b");
+	});
 });
